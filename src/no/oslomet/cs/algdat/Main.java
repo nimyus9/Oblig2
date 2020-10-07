9654affca70d91dc05e6bf66c9921de91d595073
nimyus9 <nimyus9@gmail.com>
2020-10-07 15:24:16 +0200

    laget main klasse for å teste

---
+++ src/no/oslomet/cs/algdat/Main.java
@@ -0,0 +1,8 @@
+package no.oslomet.cs.algdat;
+
+public class Main {
+    public static void main (String[] args){
+        Liste<String> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+}
