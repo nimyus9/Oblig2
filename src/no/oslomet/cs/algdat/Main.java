d50db7b4f3728a6984f9a14af8e053f3b772f70a
nimyus9 <nimyus9@gmail.com>
2020-10-08 14:33:01 +0200

    testet med main metode og gir riktig utskrift

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -2,12 +2,36 @@ package no.oslomet.cs.algdat;
 
 public class Main {
     public static void main (String[] args){
+        // Oppgave 1
         /*
         Liste<String> liste = new DobbeltLenketListe<>();
         System.out.println(liste.antall() + " " + liste.tom());
-        */
+
         String[] s = {"Ole", null, "Per", "Kari", null};
         Liste<String> liste2 = new DobbeltLenketListe<>(s);
         System.out.println(liste2.antall() + " " + liste2.tom());
+
+        */
+
+        // Oppgave 2 a)
+        /*
+        String[] s1 = {}, s2 = {"A"}, s3 = {null, "A", null, "B", null};
+        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
+        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
+        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
+
+        System.out.println(l1.toString() + " " + l2.toString() + " "
+                + l3.toString() + " " + l1.omvendtString() + " "
+                + l2.omvendtString() + " " + l3.omvendtString());
+         */
+
+        // Oppgave 2 b)
+        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.toString() + " " + liste.omvendtString());
+
+        for (int i = 1; i <= 3; i++) {
+            liste.leggInn(i);
+            System.out.println(liste.toString() + " " + liste.omvendtString());
+        }
     }
 }
