d89ecc3824a57f96b6e1631fa2d319c5cc124610
nimyus9 <nimyus9@gmail.com>
2020-10-08 14:03:05 +0200

    sjekker at main utskrift er riktig

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -2,7 +2,12 @@ package no.oslomet.cs.algdat;
 
 public class Main {
     public static void main (String[] args){
+        /*
         Liste<String> liste = new DobbeltLenketListe<>();
         System.out.println(liste.antall() + " " + liste.tom());
+        */
+        String[] s = {"Ole", null, "Per", "Kari", null};
+        Liste<String> liste2 = new DobbeltLenketListe<>(s);
+        System.out.println(liste2.antall() + " " + liste2.tom());
     }
 }
